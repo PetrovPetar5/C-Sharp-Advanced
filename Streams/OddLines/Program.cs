f79cd0cbdb7545233d8e1b61a8ea53f36d073eed
PetrovPetar5 <74185222+PetrovPetar5@users.noreply.github.com>
2021-05-02 23:28:56 +0300

    Add files via upload

---
+++ Streams/OddLines/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace OddLines
+{
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+            using (var writer = new StreamWriter($@"{Directory.GetCurrentDirectory()}\output.txt", false))
+            {
+                using (var reader = new StreamReader($@"{Directory.GetCurrentDirectory()}\input.txt"))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
