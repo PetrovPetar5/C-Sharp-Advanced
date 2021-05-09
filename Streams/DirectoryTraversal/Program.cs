43c8d1e03684ff6de4fbe1e324d3abe9e46e3375
PetrovPetar5 <74185222+PetrovPetar5@users.noreply.github.com>
2021-05-09 23:49:10 +0300

    Add files via upload

---
+++ Streams/DirectoryTraversal/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DirectoryTraversal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dirFiles = Directory.GetFiles($@"{Directory.GetCurrentDirectory()}");
+            var extensions = new Dictionary<string, HashSet<FileInfo>>();
+            foreach (var @path in dirFiles)
+            {
+
+                var fileInfo = new FileInfo(@path);
+                var curExtension = fileInfo.Extension;
+                if (!extensions.ContainsKey(curExtension))
+                {
+                    extensions[curExtension] = new HashSet<FileInfo>();
+                }
+
+                extensions[curExtension].Add(fileInfo);
+            }
+
+            extensions = extensions.OrderByDescending(x => x.Value.Count()).ThenBy(x => x.Key).ToDictionary(x => x.Key, y => y.Value);
+            using (var writer = new StreamWriter($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}report.txt"))
+            {
+                foreach (var extension in extensions)
+                {
+                    writer.WriteLine($"{extension.Key}");
+
+                    foreach (var @fileInfo in extension.Value)
+                    {
+                        writer.WriteLine($"--{@fileInfo.Name} - {@fileInfo.Length * 0.001:F3}kb");
+                    }
+                }
+            }
+        }
+    }
+}
