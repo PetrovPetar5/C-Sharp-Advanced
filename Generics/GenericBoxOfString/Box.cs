58a6c1bd3c195b6e551903dc25ff88f2ca583c46
PetrovPetar5 <74185222+PetrovPetar5@users.noreply.github.com>
2021-05-26 23:55:04 +0300

    Add files via upload

---
+++ Generics/GenericBoxOfString/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+        public T Value { get; }
+        public override string ToString()
+        {
+            var result = $"{this.Value.GetType()}: {this.Value}";
+
+            return result;
+        }
+    }
+}
---
+++ Generics/GenericBoxOfString/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericBoxOfString
+{
+    using System;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+            var inputCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputCount; i++)
+            {
+                var currentBoxValue = new Box<string>(Console.ReadLine());
+                sb.AppendLine(currentBoxValue.ToString());
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+    }
+}
