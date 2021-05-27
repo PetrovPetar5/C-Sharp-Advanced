6b3803e62a57b79094e5313cea7139a4d990fe7f
PetrovPetar5 <74185222+PetrovPetar5@users.noreply.github.com>
2021-05-27 17:18:04 +0300

    Add files via upload

---
+++ Generics/GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace GenericSwapMethodStrings
+{
+    using Box;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputCount = int.Parse(Console.ReadLine());
+            var boxes = new List<Box<string>>();
+            for (int i = 0; i < inputCount; i++)
+            {
+                var input = Console.ReadLine();
+                var curBox = new Box<string>(input);
+                boxes.Add(curBox);
+            }
+
+            var indiceInput = Console.ReadLine()
+                               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .ToArray();
+            Box<string>.Swap(indiceInput, boxes);
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
