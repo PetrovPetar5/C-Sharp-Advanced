d458c874c94684b5e3ff2e3351ce80e758f0d737
PetrovPetar5 <74185222+PetrovPetar5@users.noreply.github.com>
2021-05-28 17:35:47 +0300

    Add files via upload

---
+++ Generics/Tuple/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameAddress = Console.ReadLine()
+                .Split().ToArray();
+            var fullName = $"{nameAddress[0]} {nameAddress[1]}";
+            var address = nameAddress[2];
+
+            var beerInfo = Console.ReadLine()
+                .Split().ToArray();
+            var name = beerInfo[0];
+            var beerAmount = int.Parse(beerInfo[1]);
+
+            var numbersInfo = Console.ReadLine()
+               .Split().ToArray();
+            var someIntNum = int.Parse(numbersInfo[0]);
+            var someDoubleNum = double.Parse(numbersInfo[1]);
+
+            var nameAndAddress = new Tuplee<string, string>(fullName, address);
+            var beerLitersDrank = new Tuplee<string, int>(name, beerAmount);
+            var regularIntAndDoubleNumbers = new Tuplee<int, double>(someIntNum, someDoubleNum);
+
+            Console.WriteLine(nameAndAddress);
+            Console.WriteLine(beerLitersDrank);
+            Console.WriteLine(regularIntAndDoubleNumbers);
+        }
+    }
+}
---
+++ Generics/Tuple/Tuplee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Tuplee<TFirst, TSecond>
+    {
+        public Tuplee(TFirst firstItem, TSecond secondItem)
+        {
+            this.FirstItem = firstItem;
+            this.SecondItem = secondItem;
+        }
+        public TFirst FirstItem { get; set; }
+        public TSecond SecondItem { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ this.FirstItem} -> { this.SecondItem}";
+        }
+    }
+}
