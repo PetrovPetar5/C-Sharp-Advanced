66ae2f84f9c3d41d032762955f4811159bcd9efb
PetrovPetar5 <74185222+PetrovPetar5@users.noreply.github.com>
2021-06-03 23:50:37 +0300

    Add files via upload

---
+++ IteratorsAndComparators/ExamsAdvanced/Bee/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixSizes = int.Parse(Console.ReadLine());
+            var matrix = new char[matrixSizes, matrixSizes];
+            var beeCurLocation = new List<int>();
+            ReadsMatrix(matrix, beeCurLocation);
+            var minFlowersToCollect = 5;
+            var flowersCollected = 0;
+            var isOut = false;
+            var direction = Console.ReadLine();
+            while (direction != "End")
+            {
+                var nextRowPosition = beeCurLocation[0];
+                var nextColPosition = beeCurLocation[1];
+
+
+                switch (direction)
+                {
+                    case "up":
+                        nextRowPosition--;
+                        break;
+                    case "down":
+                        nextRowPosition++;
+                        break;
+                    case "left":
+                        nextColPosition--;
+                        break;
+                    case "right":
+                        nextColPosition++;
+                        break;
+                }
+
+                matrix[beeCurLocation[0], beeCurLocation[1]] = '.';
+                if (IsValid(matrix, nextRowPosition, nextColPosition))
+                {
+
+                    if (FlowerOrNot(matrix, ref flowersCollected, ref nextRowPosition, nextColPosition))
+                    {
+                        flowersCollected++;
+                    }
+                    else if (matrix[nextRowPosition, nextColPosition] == 'O')
+                    {
+                        matrix[nextRowPosition, nextColPosition] = '.';
+                        beeCurLocation[0] = nextRowPosition;
+                        beeCurLocation[1] = nextColPosition;
+                        continue;
+                    }
+
+                    beeCurLocation[0] = nextRowPosition;
+                    beeCurLocation[1] = nextColPosition;
+                    matrix[nextRowPosition, nextColPosition] = 'B';
+                }
+                else
+                {
+                    isOut = true;
+                    break;
+                }
+
+                direction = Console.ReadLine();
+            }
+
+            if (isOut)
+            {
+                Console.WriteLine("The bee got lost!");
+            }
+
+            if (flowersCollected >= minFlowersToCollect)
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {flowersCollected} flowers!");
+            }
+            else
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {minFlowersToCollect - flowersCollected} flowers more");
+            }
+
+            PrintsMatrix(matrix);
+        }
+
+        private static void AddsAFlower(char[,] matrix, ref int flowersCollected, ref int nextRowPosition, int nextColPosition)
+        {
+            if (FlowerOrNot(matrix, ref flowersCollected, ref nextRowPosition, nextColPosition))
+            {
+                flowersCollected++;
+            }
+        }
+
+        static bool FlowerOrNot(char[,] matrix, ref int flowersCollected, ref int nextRowPosition, int nextColPosition)
+        {
+            if (matrix[nextRowPosition, nextColPosition] == 'f')
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        static bool IsValid(char[,] matrix, int nextRow, int nextCol)
+        {
+            if (nextRow >= 0 && nextRow < matrix.GetLongLength(0) && nextCol >= 0 && nextCol < matrix.GetLength(1))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void PrintsMatrix(char[,] matrix)
+        {
+            for (int ROW = 0; ROW < matrix.GetLength(0); ROW++)
+            {
+                for (int COL = 0; COL < matrix.GetLength(1); COL++)
+                {
+                    Console.Write(matrix[ROW, COL]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void ReadsMatrix(char[,] matrix, List<int> beeCurLocation)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var rowData = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                    if (matrix[row, col] == 'B')
+                    {
+                        beeCurLocation.Add(row);
+                        beeCurLocation.Add(col);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ IteratorsAndComparators/ExamsAdvanced/Revolt/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+namespace Revolt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixSize = int.Parse(Console.ReadLine());
+            var commandsToFollow = int.Parse(Console.ReadLine());
+            var field = new char[matrixSize, matrixSize];
+            var playerRow = 0;
+            var playerCol = 0;
+            var hasWon = false;
+            ReadsMatrix(field, ref playerRow, ref playerCol);
+            for (int i = 0; i < commandsToFollow; i++)
+            {
+                var previousPlayerRow = playerRow;
+                var previousPlayerCol = playerCol;
+                var direction = Console.ReadLine();
+                PlayerFutureSpot(ref playerRow, ref playerCol, direction);
+                if (InOrOutTheField(playerRow, playerCol, field))
+                {
+                    if (field[playerRow, playerCol] == 'B')
+                    {
+                        PlayerFutureSpot(ref playerRow, ref playerCol, direction);
+                        if (!(InOrOutTheField(playerRow, playerCol, field)))
+                        {
+                            OutTheFieldMovement(field, ref playerRow, ref playerCol, direction);
+                        }
+                    }
+                    else if (field[playerRow, playerCol] == 'T')
+                    {
+                        playerRow = previousPlayerRow;
+                        playerCol = previousPlayerCol;
+                    }
+                    else if (field[playerRow, playerCol] == 'F')
+                    {
+                        field[previousPlayerRow, previousPlayerCol] = '-';
+                        field[playerRow, playerCol] = 'f';
+                        hasWon = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    OutTheFieldMovement(field, ref playerRow, ref playerCol, direction);
+                    if (field[playerRow, playerCol] == 'B')
+                    {
+                        PlayerFutureSpot(ref playerRow, ref playerCol, direction);
+                        if (!(InOrOutTheField(playerRow, playerCol, field)))
+                        {
+                            OutTheFieldMovement(field, ref playerRow, ref playerCol, direction);
+                        }
+                    }
+                    else if (field[playerRow, playerCol] == 'T')
+                    {
+                        playerRow = previousPlayerRow;
+                        playerCol = previousPlayerCol;
+                    }
+                }
+
+
+                field[previousPlayerRow, previousPlayerCol] = '-';
+                field[playerRow, playerCol] = 'f';
+            }
+
+            if (hasWon)
+            {
+                Console.WriteLine("Player won!");
+            }
+            else
+            {
+                Console.WriteLine("Player lost!");
+            }
+
+            PrintsMatrix(field);
+        }
+
+        private static void OutTheFieldMovement(char[,] field, ref int playerRow, ref int playerCol, string direction)
+        {
+            if (direction == "up")
+            {
+                playerRow = field.GetLength(0) - 1;
+            }
+            else if (direction == "down")
+            {
+                playerRow = 0;
+            }
+            else if (direction == "left")
+            {
+                playerCol = field.GetLength(1) - 1;
+            }
+            else if (direction == "right")
+            {
+                playerCol = 0;
+            }
+        }
+
+        static bool InOrOutTheField(int playerRow, int playerCol, char[,] field)
+        {
+            if (playerRow >= 0 && playerRow < field.GetLength(0) && playerCol >= 0 && playerCol < field.GetLength(1))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static void PlayerFutureSpot(ref int playerRow, ref int playerCol, string direction)
+        {
+            if (direction == "up")
+            {
+                playerRow--;
+            }
+            else if (direction == "down")
+            {
+                playerRow++;
+            }
+            else if (direction == "left")
+            {
+                playerCol--;
+            }
+            else if (direction == "right")
+            {
+                playerCol++;
+            }
+        }
+
+        private static void PrintsMatrix(char[,] field)
+        {
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    Console.Write(field[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void ReadsMatrix(char[,] field, ref int playerRow, ref int playerCol)
+        {
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                var rowData = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    field[row, col] = rowData[col];
+                    if (field[row, col] == 'f')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+        }
+    }
+}
