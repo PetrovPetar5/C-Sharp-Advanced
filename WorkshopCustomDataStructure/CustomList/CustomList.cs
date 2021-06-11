5f728b21f44d696cd98e295f6a322c09ece9f225
PetrovPetar5 <74185222+PetrovPetar5@users.noreply.github.com>
2021-06-11 22:59:31 +0300

    Add files via upload

---
+++ WorkshopCustomDataStructure/CustomList/CustomList.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class CustomList<T>
+    {
+        private T[] items;
+
+        public CustomList()
+        {
+            this.items = new T[2];
+        }
+
+
+    }
+}
