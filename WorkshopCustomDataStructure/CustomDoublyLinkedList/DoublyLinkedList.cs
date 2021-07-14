f29e6b706f61eb09291dbf0455aa1f13a12be39a
PetrovPetar5 <74185222+PetrovPetar5@users.noreply.github.com>
2021-07-14 23:23:40 +0300

    Add files via upload

--- WorkshopCustomDataStructure/CustomDoublyLinkedList/DoublyLinkedList.cs
+++ WorkshopCustomDataStructure/CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -47,9 +47,63 @@
             this.Count++;
         }
 
-        public void RemoveFirst()
+        public T RemoveFirst()
         {
+            if (this.Count == 0)
+            {
+                throw new NullReferenceException("The list is empty");
+            }
 
+            var firstElement = this.head.Value;
+            this.head = this.head.NextNode;
+            if (this.head != null)
+            {
+                this.head.PreviousNode = null;
+            }
+            else
+            {
+                this.tail = null;
+            }
+
+            this.Count--;
+            return firstElement;
+        }
+
+        public T RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new ArgumentNullException("The List is empty.");
+            }
+
+            var lastElement = this.tail.Value;
+            this.tail = this.tail.PreviousNode;
+            if (this.tail != null)
+            {
+                this.tail.NextNode = null;
+            }
+            else
+            {
+                this.head = null;
+            }
+
+            this.Count--;
+            return lastElement;
+        }
+
+        public T[] ToAarray()
+        {
+            var arrayT = new T[this.Count];
+            var counter = 0;
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                arrayT[counter] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+                counter++;
+            }
+
+            return arrayT;
         }
 
         private class ListNode<T>
